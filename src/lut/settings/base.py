1a1297922607ba727050d3c6420f6801dc0983a9
dererumnatura <pollindrom@gmail.com>
2018-10-24 17:05:06 +0300

    add css

--- src/lut/settings/base.py
+++ src/lut/settings/base.py
@@ -17,6 +17,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+SETTINGS_EXPORT = [
+    'MEDIA_ROOT',
+    'BASE_DIR'
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -65,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django_settings_export.settings_export',
             ],
         },
     },
@@ -121,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( # todo change. Must to be added to use css
+  os.path.join(BASE_DIR, 'static/'),
+)
